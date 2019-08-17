a9fe21c7af51b5dab87cd9c3f26175fae6b33e15
luluxiaocc <516987327@qq.com>
2019-08-17 08:56:28 +0800

    笔记整理

--- config/build.js
+++ config/build.js
@@ -1,13 +1,3 @@
 module.exports = {
   mode: 'production'
-  //  plugins: [
-  //  压缩js代码
-  //  new UglifyJsPlugin(/* ... */),
-  //  改环境变量
-  //  new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
-  //  过去 webpack 打包时的一个取舍是将 bundle 中各个模块单独打包成闭包。这些打包函数使你的 JavaScript 在浏览器中处理的更慢。相比之下，这个插件 可以提升或者预编译所有模块到一个闭包中，提升你的代码在浏览器中的执行速度。
-  //  new webpack.optimize.ModuleConcatenationPlugin(),
-  //  在编译出现错误时，使用 NoEmitOnErrorsPlugin 来跳过输出阶段。这样可以确保输出资源不会包含错误。对于所有资源，统计资料(stat)的 emitted 标识都是 false。
-  //   new webpack.NoEmitOnErrorsPlugin()
-  //  ]
 };
--- config/dev.js
+++ config/dev.js
@@ -2,44 +2,18 @@ const path = require('path');
 const Webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  // 知识点1: mode的区别
-    // ## development
-    // 1. 不压缩
-    // 2. 不摇树
-    // 3. 有完善的错误提示
-    // 4. 可以设完备的source-map
-    // new webpack.NamedModulesPlugin(),
-    // new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") }),
-    // 记住，只设置 NODE_ENV，则不会自动设置 mode。
-  mode: 'development', // 不要模式可以设置 "none"
-  // 知识点2: map的各种选项
+  mode: 'development',
   devtool: 'source-map',
-  // 知识点3: 各种配置
   devServer: {
     port: 9999,
     hot: true
   },
-  resolve: {
-    // 引用包时候的路径
-    // 比如说require('cc'), 那么会从如下配置路径去找有没有cc模块
-    // 使自己写的导出方法, 也可以不指定路径, 直接引用
-    // modules: [path.resolve(__dirname, '../node_modules')],
-    // alias: { // 别名
-    //   '@': './dist/home.js'
-    // },
-    // mainFiles: ['index', 'main'], // 先找index, 没有再找main文件
-    // extensions: ['js', 'css'] // 扩展名先找js, 没有的话再找css
-  },
   plugins: [
-    // 这个插件的作用是在热加载时直接返回更新文件名，而不是文件的id。
-    // 当开启 HMR 的时候使用该插件会显示模块的相对路径，建议用于开发环境。
-    // development已经开启了
-    // new Webpack.NamedModulesPlugin(),
     new Webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, '../use/1:模板.html'),
-      inject: 'head' // 插入头部
+      inject: 'head'
     })
   ]
 };
